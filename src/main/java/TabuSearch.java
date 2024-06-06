c381ac0c54b0a9ef494d2f48903aacbdee485026
Silvian Gulica <gulica.sv@gmail.com>
2024-06-07 02:57:47 +0300

    could be better ...

--- src/main/java/TabuSearch.java
+++ src/main/java/TabuSearch.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TabuSearch {
@@ -6,6 +7,8 @@ public class TabuSearch {
     private final List<Period> periods;
     private final ConstraintChecker hardConstraintChecker;
     private final ConstraintChecker softConstraintChecker;
+    private final List<List<Lecture>> tabuList;
+    private final int tabuTenure;
 
     public TabuSearch(List<Period> periods, List<Classroom> classrooms) {
         this.classrooms = classrooms;
@@ -22,34 +25,83 @@ public class TabuSearch {
         this.softConstraintChecker.addConstraint(new FirstTwoWeekDayCourseConstraint());
         this.softConstraintChecker.addConstraint(new FitClassroomConstraint());
 
-
+        this.tabuList = new LinkedList<>();
+        this.tabuTenure = 10; // or any other appropriate value
     }
 
+//    public void runTabuSearch(List<Lecture> rawSolution) {
+//        // implemeting the tabusearch
+//        var solution = rawSolution;
+//        var optimalSolution = rawSolution;
+//
+//        double k = 0;
+//        double ratio = 0.1;
+//        var max_itter = 20000;
+//        double ratio_max = 1.0;
+//        double ratio_min = 0.05;
+//
+//        while (k <= max_itter) {
+//            List<List<Lecture>> listOfNeighbours = new ArrayList<>();
+//
+//            // generate neighbours
+//            listOfNeighbours.add(relocate(solution, ratio));
+//            listOfNeighbours.add(swap(solution, ratio));
+//            listOfNeighbours.add(swap(solution));
+//            listOfNeighbours.add(relocate(solution));
+//
+//
+//            // cautam acel vecin care are energia cea mai mica pentru hard + soft
+//            var bestNeighbour = listOfNeighbours.stream()
+//                    .min((l1, l2) -> {
+//                        var score1 = hardConstraintChecker.checkConstraints(l1) + softConstraintChecker.checkConstraints(l1);
+//                        var score2 = hardConstraintChecker.checkConstraints(l2) + softConstraintChecker.checkConstraints(l2);
+//                        return score1 - score2;
+//                    }).get();
+//
+//            // daca vecinul gasit este mai bun decat solutia curenta, atunci il setam ca solutie
+//            var bestNeighbourScore = hardConstraintChecker.checkConstraints(bestNeighbour) + softConstraintChecker.checkConstraints(bestNeighbour);
+//            var bestSolutionScore = hardConstraintChecker.checkConstraints(optimalSolution) + softConstraintChecker.checkConstraints(optimalSolution);
+//
+//            if (bestNeighbourScore < bestSolutionScore) {
+//                optimalSolution = bestNeighbour;
+//                System.out.println("\nIteration: " + k);
+//                System.out.println("Ratio: " + ratio);
+//                System.out.println("bestSolutionScore: " + bestSolutionScore);
+//            }
+//            solution = optimalSolution;
+//
+//            // update the ratio
+//            // first using the linear update
+//            //ratio = k / max_itter * (ratio_max - ratio_min) + ratio_min;
+//
+//            ratio = ratio_min * Math.pow(ratio_max / ratio_min, k / max_itter);
+//            k += ratio;
+//        }
+//
+//        // generate the html document
+//        Utils.generateHTMLDocumentForLectures(optimalSolution);
+//    }
+
     public void runTabuSearch(List<Lecture> rawSolution) {
-        // implemeting the tabusearch
+        // implementing the tabu search
         var solution = rawSolution;
         var optimalSolution = rawSolution;
 
         double k = 0;
         double ratio = 0.1;
-        var max_itter = 20000;
-        double ratio_max = 0.5;
-        double ratio_min = 0.005;
-        int k_int = 0;
+        var max_iter = 100000;
+        double ratio_max = 1.0;
+        double ratio_min = 0.05;
 
-        while (k <= max_itter) {
+        while (k <= max_iter) {
             List<List<Lecture>> listOfNeighbours = new ArrayList<>();
 
             // generate neighbours
             listOfNeighbours.add(relocate(solution, ratio));
-            listOfNeighbours.add(relocate(solution));
-            listOfNeighbours.add(swap(solution));
-            listOfNeighbours.add(ejectionChain(solution, ratio));
-            listOfNeighbours.add(ejectionChain(solution));
             listOfNeighbours.add(swap(solution, ratio));
+//            listOfNeighbours.add(swap(solution));
+//            listOfNeighbours.add(relocate(solution));
 
-
-            // cautam acel vecin care are energia cea mai mica pentru hard + soft
             var bestNeighbour = listOfNeighbours.stream()
                     .min((l1, l2) -> {
                         var score1 = hardConstraintChecker.checkConstraints(l1) + softConstraintChecker.checkConstraints(l1);
@@ -57,63 +109,51 @@ public class TabuSearch {
                         return score1 - score2;
                     }).get();
 
-            // daca vecinul gasit este mai bun decat solutia curenta, atunci il setam ca solutie
             var bestNeighbourScore = hardConstraintChecker.checkConstraints(bestNeighbour) + softConstraintChecker.checkConstraints(bestNeighbour);
             var bestSolutionScore = hardConstraintChecker.checkConstraints(optimalSolution) + softConstraintChecker.checkConstraints(optimalSolution);
 
-            if (bestNeighbourScore < bestSolutionScore) {
-                optimalSolution = bestNeighbour;
-                System.out.println("\nIteration: " + k);
-                System.out.println("Ratio: " + ratio);
-                System.out.println("bestSolutionScore: " + bestSolutionScore);
+            boolean isTabu = tabuList.contains(bestNeighbour);
+            boolean isAspirationCriteriaMet = bestNeighbourScore < bestSolutionScore;
+
+            if (!isTabu || isAspirationCriteriaMet) {
+                if (bestNeighbourScore < bestSolutionScore) {
+                    optimalSolution = bestNeighbour;
+                    System.out.println("\nIteration: " + k);
+                    System.out.println("Ratio: " + ratio);
+                    System.out.println("bestSolutionScore: " + bestSolutionScore);
+                }
+                solution = optimalSolution;
+
+                tabuList.add(new ArrayList<>(bestNeighbour));
+                if (tabuList.size() > tabuTenure) {
+                    tabuList.removeFirst();
+                }
             }
-            solution = optimalSolution;
 
             // update the ratio
             // first using the linear update
-            ratio = k / max_itter * (ratio_max - ratio_min) + ratio_min;
-//            ratio = ratio_min * Math.pow(ratio_max / ratio_min, k / max_itter);
+            // ratio = k / max_iter * (ratio_max - ratio_min) + ratio_min;
+
+            ratio = ratio_min * Math.pow(ratio_max / ratio_min, k / max_iter);
             k += ratio;
-            k_int++;
-
-            if (k_int % 10000 == 0) {
-                System.out.println("Iteration: " + k_int);
-            }
-
         }
 
         // generate the html document
         Utils.generateHTMLDocumentForLectures(optimalSolution);
     }
 
-    public List<Lecture> relocate(List<Lecture> lectures) {
-
-
-        var possibleSolution = Utils.deepCopy(lectures);
-
-        // pentru toate selected lectures, se incearca sa se mute in alta perioada si sala
-        // respectand hard constraints
-        for (var lecture : possibleSolution) {
-            for (var period : periods) {
-                for (var classroom : classrooms) {
-                    // daca perioada si sala nu sunt ocupate de alt lecture
-                    if (possibleSolution.stream().noneMatch(lec -> lec.allocatedPeriod.equals(period) && lec.classroom.equals(classroom))) {
-                        lecture.allocatedPeriod = period;
-                        lecture.classroom = classroom;
-                    }
-                }
-            }
-        }
-
-        return possibleSolution;
-    }
 
     public List<Lecture> relocate(List<Lecture> lectures, double ratio) {
         var possibleSolution = Utils.deepCopy(lectures);
         var selectedLectures = new ArrayList<Lecture>();
-        for (int i = 0; i < Math.ceil(ratio * lectures.size()); i++) {
+
+        var randomRange = Math.ceil(ratio / lectures.size());
+
+        for (int i = 0; i < randomRange; i++) {
             var randomIndex = (int) (Math.random() * lectures.size());
-            selectedLectures.add(possibleSolution.get(randomIndex));
+            if (selectedLectures.stream().noneMatch(lec -> lec.equals(possibleSolution.get(randomIndex)))) {
+                selectedLectures.add(possibleSolution.get(randomIndex));
+            }
         }
 
         for (var lecture : selectedLectures) {
@@ -130,6 +170,22 @@ public class TabuSearch {
         return possibleSolution;
     }
 
+    public List<Lecture> relocate(List<Lecture> lectures) {
+        var possibleSolution = Utils.deepCopy(lectures);
+
+        for (var lecture : possibleSolution) {
+            for (var period : periods) {
+                for (var classroom : classrooms) {
+                    if (possibleSolution.stream().noneMatch(lec -> lec.allocatedPeriod.equals(period) && lec.classroom.equals(classroom))) {
+                        lecture.allocatedPeriod = period;
+                        lecture.classroom = classroom;
+                    }
+                }
+            }
+        }
+
+        return possibleSolution;
+    }
 
     public List<Lecture> swap(List<Lecture> lectures) {
         var possibleSolution = Utils.deepCopy(lectures);
@@ -156,77 +212,98 @@ public class TabuSearch {
     public List<Lecture> swap(List<Lecture> lectures, double ratio) {
         var possibleSolution = Utils.deepCopy(lectures);
 
-        // Schimba doua lecturi cu locu pentru ratio * n * ((n-1) / 2) perechi
-        for (int i = 0; i < Math.ceil(ratio * lectures.size() * (lectures.size() - 1) / 2); i++) {
-            var randomIndex1 = (int) (Math.random() * lectures.size());
-            var randomIndex2 = (int) (Math.random() * lectures.size());
+        var n = lectures.size();
+        var randomRange = Math.ceil((double) 1 /2 * (2*n - 1 - Math.sqrt(4*n*(n-1) * (1 - ratio) + 1)));
 
-            var tempPeriod = possibleSolution.get(randomIndex1).allocatedPeriod;
-            var tempClassroom = possibleSolution.get(randomIndex1).classroom;
+        var selectedLectures = new ArrayList<Lecture>();
+        for (int i = 0; i < randomRange; i++) {
+            var randomIndex = (int) (Math.random() * lectures.size());
+            if (selectedLectures.stream().noneMatch(lec -> lec.equals(possibleSolution.get(randomIndex)))) {
+                selectedLectures.add(possibleSolution.get(randomIndex));
+            }
+        }
 
-            possibleSolution.get(randomIndex1).allocatedPeriod = possibleSolution.get(randomIndex2).allocatedPeriod;
-            possibleSolution.get(randomIndex1).classroom = possibleSolution.get(randomIndex2).classroom;
+        var selectedLectures2 = new ArrayList<Lecture>();
+        for (int i = 0; i < randomRange; i++) {
+            var randomIndex = (int) (Math.random() * lectures.size());
+            // verifica sa nu fie deja selectata si sa nu fie aceeasi cu prima
+            if (selectedLectures.stream().noneMatch(lec -> lec.equals(possibleSolution.get(randomIndex))) &&
+                    selectedLectures2.stream().noneMatch(lec -> lec.equals(possibleSolution.get(randomIndex)))) {
+                selectedLectures2.add(possibleSolution.get(randomIndex));
+            }
+        }
 
-            possibleSolution.get(randomIndex2).allocatedPeriod = tempPeriod;
-            possibleSolution.get(randomIndex2).classroom = tempClassroom;
+        for (var lec1 : selectedLectures) {
+            for (var lec2 : selectedLectures2) {
+                if (lec1 != lec2) {
+                    var tempPeriod = lec1.allocatedPeriod;
+                    var tempClassroom = lec1.classroom;
+
+                    lec1.allocatedPeriod = lec2.allocatedPeriod;
+                    lec1.classroom = lec2.classroom;
+
+                    lec2.allocatedPeriod = tempPeriod;
+                    lec2.classroom = tempClassroom;
+                }
+            }
         }
 
 
         return possibleSolution;
     }
 
-    public List<Lecture> ejectionChain(List<Lecture> lectures, double ratio) {
-        var possibleResults = Utils.deepCopy(lectures);
-
-        // selecteaza random ratio * n lectures
-        var selectedLectures = new ArrayList<Lecture>();
-        for (int i = 0; i < Math.ceil(ratio * lectures.size()); i++) {
-            var randomIndex = (int) (Math.random() * lectures.size());
-            selectedLectures.add(possibleResults.get(randomIndex));
-        }
-
-        // pentru fiecare selected lecture, se incearca sa se mute in alta perioada si sala,
-        // dar daca energia configuratiei hard este == 0 sau daca exista mai mult de 2 conflicte ne oprim
-        // in schimb daca exista un conflict, se incearca sa se mute in alta perioada si sala
-        for (var lecture : selectedLectures) {
-            for (var newPeriod : periods) {
-                for (var newClassroom : classrooms) {
-                    if (possibleResults.stream().noneMatch(lec -> lec.allocatedPeriod.equals(newPeriod) && lec.classroom.equals(newClassroom))) {
-                        lecture.allocatedPeriod = newPeriod;
-                        lecture.classroom = newClassroom;
-
-                        if (hardConstraintChecker.checkConstraints(possibleResults) == 0) {
-                            return possibleResults;
-                        }
-                    }
-                }
-            }
-        }
-
-        return possibleResults;
-    }
-
-    public List<Lecture> ejectionChain(List<Lecture> lectures) {
-        var possibleResults = Utils.deepCopy(lectures);
-
-        // incearca sa mute fiecare lecture catre alta perioada si sala.
-        // daca primeste hardconstraint == 0 sau daca are mai mult de 2 conflicte, se opreste si returneaza
-        // daca are doar 1 confilict, incearca sa il rezolve
-        for (var lecture : possibleResults) {
-            for (var newPeriod : periods) {
-                for (var newClassroom : classrooms) {
-                    if (possibleResults.stream().noneMatch(lec -> lec.allocatedPeriod.equals(newPeriod) && lec.classroom.equals(newClassroom))) {
-                        lecture.allocatedPeriod = newPeriod;
-                        lecture.classroom = newClassroom;
-
-                        if (hardConstraintChecker.checkConstraints(possibleResults) == 0) {
-                            return possibleResults;
-                        }
-                    }
-                }
-            }
-        }
-
-        return possibleResults;
-    }
+//    public List<Lecture> ejectionChain(List<Lecture> lectures, double ratio) {
+//        var possibleResults = Utils.deepCopy(lectures);
+//
+//        // selecteaza random ratio * n lectures
+//        var selectedLectures = new ArrayList<Lecture>();
+//        for (int i = 0; i < Math.ceil(ratio * lectures.size()); i++) {
+//            var randomIndex = (int) (Math.random() * lectures.size());
+//            selectedLectures.add(possibleResults.get(randomIndex));
+//        }
+//
+//        // pentru fiecare selected lecture, se incearca sa se mute in alta perioada si sala,
+//        // dar daca energia configuratiei hard este == 0 sau daca exista mai mult de 2 conflicte ne oprim
+//        // in schimb daca exista un conflict, se incearca sa se mute in alta perioada si sala
+//        for (var lecture : selectedLectures) {
+//            for (var newPeriod : periods) {
+//                for (var newClassroom : classrooms) {
+//                    if (possibleResults.stream().noneMatch(lec -> lec.allocatedPeriod.equals(newPeriod) && lec.classroom.equals(newClassroom))) {
+//                        lecture.allocatedPeriod = newPeriod;
+//                        lecture.classroom = newClassroom;
+//
+//                        if (hardConstraintChecker.checkConstraints(possibleResults) == 0) {
+//                            return possibleResults;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        return possibleResults;
+//    }
+//
+//    public List<Lecture> ejectionChain(List<Lecture> lectures) {
+//        var possibleResults = Utils.deepCopy(lectures);
+//
+//        // incearca sa mute fiecare lecture catre alta perioada si sala.
+//        // daca primeste hardconstraint == 0 sau daca are mai mult de 2 conflicte, se opreste si returneaza
+//        // daca are doar 1 confilict, incearca sa il rezolve
+//        for (var lecture : possibleResults) {
+//            for (var newPeriod : periods) {
+//                for (var newClassroom : classrooms) {
+//                    if (possibleResults.stream().noneMatch(lec -> lec.allocatedPeriod.equals(newPeriod) && lec.classroom.equals(newClassroom))) {
+//                        lecture.allocatedPeriod = newPeriod;
+//                        lecture.classroom = newClassroom;
+//
+//                        if (hardConstraintChecker.checkConstraints(possibleResults) == 0) {
+//                            return possibleResults;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        return possibleResults;
+//    }
 }
